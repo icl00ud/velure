6754b68c1ee1a8033bac24c6c75bc1abc741985a
Israel Schroeder Moreira <israelschroederm@gmail.com>
2024-03-05 23:45:19 -0300

    Fifth Commit

--- velure-store-ui/src/app/app.component.ts
+++ velure-store-ui/src/app/app.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { HeaderComponent } from "./components/header/header.component";
+
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.less'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.less'],
+    imports: [
+        CommonModule,
+        RouterOutlet,
+        HeaderComponent
+    ]
 })
-export class AppComponent { }
+export class AppComponent {
+  isCollapsed = false;
+}
---
+++ velure-store-ui/src/app/components/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+import { NzIconModule } from 'ng-zorro-antd/icon';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.less'
+})
+export class HeaderComponent {
+
+}
