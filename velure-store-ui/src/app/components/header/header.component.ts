e8fe1355613d705168edff6a453684e8cad4d89b
Israel Schroeder Moreira <israelschroederm@gmail.com>
2024-03-06 22:37:10 -0300

    Sixth Commit
    Fixed angular icons requiring HttpClientModule

--- velure-store-ui/src/app/components/header/header.component.ts
+++ velure-store-ui/src/app/components/header/header.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    NzLayoutModule,
+    NzIconModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.less'
 })
