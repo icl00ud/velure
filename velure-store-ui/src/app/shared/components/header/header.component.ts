7a2f84fc85575ff024ba4de1c1e35147a5232f4b
Israel Schroeder Moreira <israelschroederm@gmail.com>
2024-03-07 18:49:33 -0300

    Seventh Commit
    Fixed header

--- velure-store-ui/src/app/shared/components/header/header.component.ts
+++ velure-store-ui/src/app/shared/components/header/header.component.ts
@@ -1,18 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
+    CommonModule,
     NzLayoutModule,
-    NzIconModule
+    NzIconModule,
+    NzMenuModule
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.less'
 })
 export class HeaderComponent {
+  hoveringProduct: boolean = false;
 
+  constructor() {}
+
+  hoverProducts(hovering: boolean) {
+    this.hoveringProduct = hovering;
+  }
 }
