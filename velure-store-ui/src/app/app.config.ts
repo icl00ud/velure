d1e451ebadcf497811041790e609b1ccaf121480
Israel Schroeder Moreira <israelschroederm@gmail.com>
2024-03-07 21:02:33 -0300

    Eighth Commit
    Fixed nz-submenu because animations was not provided

--- velure-store-ui/src/app/app.config.ts
+++ velure-store-ui/src/app/app.config.ts
@@ -1,13 +1,14 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { HttpClientModule } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    importProvidersFrom(HttpClientModule)
+    importProvidersFrom(HttpClientModule),
+    provideAnimations()
   ]
 };
