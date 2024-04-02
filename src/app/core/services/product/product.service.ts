07f5bea763ba20d69ba01681b60b45ff34912334
Israel Schroeder <israelschroederm@gmail.com>
2024-04-01 23:10:39 -0300

    Update imports

--- src/app/core/services/product/product.service.ts
+++ src/app/core/services/product/product.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-import { Product } from '../../../shared/interface/product.interface';
+import { Product } from '../../../utils/interfaces/product.interface';
 
 import { ConfigService } from '../config/config.service';
 
