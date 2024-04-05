a9d0bc44568dde313b63aa6cf3d68df2ccccdc59
Israel Schroeder Moreira <israelschroederm@gmail.com>
2024-04-04 23:56:03 -0300

    Update imports

--- src/app/core/config/config.service.ts
+++ src/app/core/config/config.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ConfigService {
-  productApiUrl: string = 'https://localhost:3000';
+  productApiUrl: string = 'http://localhost:3000/product';
 
   constructor() { }
 }
