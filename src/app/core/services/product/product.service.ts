ce683b83f9077ac6d6976a69de18054319cdf737
Israel Schroeder Moreira <israelschroederm@gmail.com>
2024-04-04 22:21:06 -0300

    Create service to get products by page

--- src/app/core/services/product/product.service.ts
+++ src/app/core/services/product/product.service.ts
@@ -10,35 +10,36 @@ import { ConfigService } from '../config/config.service';
     providedIn: 'root'
 })
 export class ProductService {
-    private apiUrl = 'https://api.example.com/products';
-
     constructor(
         private http: HttpClient,
         private config: ConfigService
-    ) { 
-
-    }
+    ) { }
 
     getProducts(): Observable<Product[]> {
-        return this.http.get<Product[]>(this.apiUrl);
+        return this.http.get<Product[]>(this.config.productApiUrl);
     }
 
     getProductById(id: number): Observable<Product> {
-        const url = `${this.apiUrl}/${id}`;
+        const url = `${this.config.productApiUrl}/${id}`;
         return this.http.get<Product>(url);
     }
 
+    getProductsByPage(page: number, pageSize: number): Observable<Product[]> {
+        const url = `${this.config.productApiUrl}/v1/GetProductsByPage?page=${page}&pageSize=${pageSize}`;
+        return this.http.get<Product[]>(url);
+    }
+
     createProduct(product: any): Observable<any> {
-        return this.http.post<any>(this.apiUrl, product);
+        return this.http.post<any>(this.config.productApiUrl, product);
     }
 
     updateProduct(id: number, product: any): Observable<any> {
-        const url = `${this.apiUrl}/${id}`;
+        const url = `${this.config.productApiUrl}/${id}`;
         return this.http.put<any>(url, product);
     }
 
     deleteProduct(id: number): Observable<any> {
-        const url = `${this.apiUrl}/${id}`;
+        const url = `${this.config.productApiUrl}/${id}`;
         return this.http.delete<any>(url);
     }
 }
