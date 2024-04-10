c498c1bcc62363c91f767e2fe66342f2a0377113
Israel Schroeder Moreira <israelschroederm@gmail.com>
2024-04-09 23:19:55 -0300

    Created Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './shared/pages/home/home.component';
+import { PageNotFoundComponent } from './shared/pages/page-not-found/page-not-found.component';
+import { LoginComponent } from './shared/pages/login/login.component';
+
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full'},
+    { path: 'home', component: HomeComponent },
+    { path: 'login', component: LoginComponent },
+    { path: '**', component: PageNotFoundComponent},
+];
