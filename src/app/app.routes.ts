1e132b7c04da718a7433790b3cac17033f2b75c2
Israel Schroeder Moreira <israelschroederm@gmail.com>
2024-04-15 15:52:00 -0300

    Added routes to products category page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,22 @@
 import { Routes } from '@angular/router';
 
-import { HomeComponent } from './shared/pages/home/home.component';
-import { PageNotFoundComponent } from './shared/pages/page-not-found/page-not-found.component';
-import { LoginComponent } from './shared/pages/login/login.component';
+import { HomeComponent } from './core/components/home/home.component';
+import { PageNotFoundComponent } from './core/components/page-not-found/page-not-found.component';
+import { LoginComponent } from './core/components/login/login.component';
+import { CategoryProductsComponent } from './core/components/category-products/category-products.component';
+
+const categoryData = [
+  { path: 'shoes', data: { category: 'shoes' } },
+  { path: 'shirts', data: { category: 'shirts' } },
+];
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'home', pathMatch: 'full'},
-    { path: 'home', component: HomeComponent },
-    { path: 'login', component: LoginComponent },
-    { path: '**', component: PageNotFoundComponent},
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  ...categoryData.map((category) => ({
+    ...category,
+    component: CategoryProductsComponent,
+  })),
+  { path: '**', component: PageNotFoundComponent },
 ];
