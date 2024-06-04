e6dd4c6d62d33d18879f8fab398a0de796bf323d
Israel Schroeder Moreira <israelschroederm@gmail.com>
2024-06-04 20:35:14 -0300

    Created product page

---
+++ src/app/core/pages/product-page/product-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './product-page.component.html',
+  styleUrl: './product-page.component.less'
+})
+export class ProductPageComponent {
+
+}
