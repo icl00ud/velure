3e44651491bbe0cf707ca1989adb7a667bf56ac7
Israel Schroeder <israelschroederm@gmail.com>
2024-07-04 00:38:28 -0300

    Prometheus and grafana docker

--- src/app/core/config/config.service.ts
+++ src/app/core/config/config.service.ts
@@ -4,8 +4,8 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ConfigService {
-  productServiceApiUrl: string = 'http://localhost:3000/api/products';
-  authenticationServiceApiUrl: string = 'http://localhost:3000/api/auth';
+  productServiceApiUrl: string = 'http://localhost:3010/product';
+  authenticationServiceApiUrl: string = 'http://localhost:3020/authentication';
 
   constructor() { }
 }
