adc9a4bf04ef13dcd4468661d5e0b656bc2d3739
Israel Schroeder Moreira <israelschroederm@gmail.com>
2024-12-13 21:08:39 -0300

    Squashed 'velure-product-service/' changes from 013bc65..7ba8c6c
    7ba8c6c fix
    01fa2d1 fix
    7e4d2f5 added health check
    34c00c8 changes .env
    
    git-subtree-dir: velure-product-service
    git-subtree-split: 7ba8c6ca33f35eb3f87e06389f2ad926e2e9804f

--- src/providers/mongoose/mongoose.provider.ts
+++ src/providers/mongoose/mongoose.provider.ts
@@ -9,42 +9,31 @@ export const databaseProviders = [
       const logger = new Logger('MongoDB');
 
       const {
-        MONGODB_HOST = process.env.MONGODB_HOST || 'localhost',
-        MONGODB_FALLBACK_HOST = process.env.MONGODB_FALLBACK_HOST || 'localhost',
-        MONGODB_USER,
-        MONGODB_PASSWORD,
-        MONGODB_DBNAME,
+        MONGODB_HOST = 'localhost',
+        MONGODB_USER = 'velure_user',
+        MONGODB_PASSWORD = 'velure_password',
+        MONGODB_DBNAME = 'velure_database',
         MONGODB_PORT = '27017',
       } = process.env;
 
       const createConnectionString = (host: string): string => {
         if (MONGODB_USER && MONGODB_PASSWORD) {
-          return `mongodb://${encodeURIComponent(MONGODB_USER)}:${encodeURIComponent(MONGODB_PASSWORD)}@${host}:${MONGODB_PORT}/${MONGODB_DBNAME}?authSource=admin`;
+          return `mongodb+srv://${encodeURIComponent(MONGODB_USER)}:${encodeURIComponent(MONGODB_PASSWORD)}@${host}/${MONGODB_DBNAME}`;
         }
 
         return `mongodb://${host}:${MONGODB_PORT}/${MONGODB_DBNAME}`;
       };
 
       const primaryUrl = createConnectionString(MONGODB_HOST);
-      const fallbackUrl = createConnectionString(MONGODB_FALLBACK_HOST);
 
       try {
-        logger.warn(`Tentando conectar ao MongoDB primário em ${MONGODB_HOST}:${MONGODB_PORT}`);
+        logger.warn(`Tentando conectar ao MongoDB em ${MONGODB_HOST}:${MONGODB_PORT}`);
+        logger.error(`${primaryUrl}`);
         await mongoose.connect(primaryUrl);
-        logger.log('Conectado ao MongoDB primário com sucesso');
+        logger.log('Conectado ao MongoDB com sucesso');
         return mongoose;
       } catch (primaryError) {
-        logger.error(`Falha ao conectar ao MongoDB primário`);
-
-        try {
-          logger.log(`Tentando conectar ao MongoDB de fallback em ${MONGODB_FALLBACK_HOST}:${MONGODB_PORT}`);
-          await mongoose.connect(fallbackUrl);
-          logger.log('Conectado ao MongoDB de fallback com sucesso');
-          return mongoose;
-        } catch (fallbackError) {
-          logger.error(`Falha ao conectar ao MongoDB de fallback`);
-          throw new Error('Não foi possível conectar ao MongoDB (tanto o primário quanto o fallback falharam)');
-        }
+        logger.error(`Falha ao conectar ao MongoDB`);
       }
     },
   },
