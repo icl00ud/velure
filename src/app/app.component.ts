74dc193dc15f42c4bc33b5e26d61090cd6b85fe7
Israel Schroeder Moreira <israelschroederm@gmail.com>
2025-01-19 19:10:04 -0300

    Squashed 'velure-ui-service/' changes from 92ad016..ad97be6
    ad97be6 a
    34da8b0 a
    c546a86 removed authguard from home page
    d6fc04d started created cart
    
    git-subtree-dir: velure-ui-service
    git-subtree-split: ad97be69bf25fef2dcaec72bfbddee636cf6f2d4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,6 @@ import { RouterOutlet } from '@angular/router';
 
 import { HeaderComponent } from "./shared/components/header/header.component";
 import { FooterComponent } from './shared/components/footer/footer.component';
-import { CarouselComponent } from './shared/components/carousel/carousel.component';
-import { ProductCardComponent } from './shared/components/product-card/product-card.component';
-import { ProductsTabComponent } from './shared/components/products-tab/products-tab.component';
-
 @Component({
     selector: 'app-root',
     standalone: true,
@@ -17,10 +13,7 @@ import { ProductsTabComponent } from './shared/components/products-tab/products-
         CommonModule,
         RouterOutlet,
         HeaderComponent,
-        FooterComponent,
-        CarouselComponent,
-        ProductCardComponent,
-        ProductsTabComponent
+        FooterComponent
     ]
 })
 export class AppComponent {
