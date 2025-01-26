12b786675ee4446a4005baf33a71714de5897234
Israel Schroeder Moreira <israelschroederm@gmail.com>
2025-01-26 17:58:52 -0300

    Remove velure services

--- velure-product-service/src/providers/mongoose/mongoose.module.ts
+++
@@ -1,9 +0,0 @@
-
-import { Module } from '@nestjs/common';
-import { databaseProviders } from './mongoose.provider';
-
-@Module({
-  providers: [...databaseProviders],
-  exports: [...databaseProviders],
-})
-export class MoongoseModule {}
