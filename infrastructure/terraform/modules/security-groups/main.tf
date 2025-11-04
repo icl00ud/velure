0a7309ad9e44335213ea7f8beb340f55f361a680
Israel Schroeder Moreira <israels@weg.net>
2025-11-04 20:11:22 -0300

    fix(terraform): corrigir security group para usar ports 0 com protocol -1
    Quando usamos protocol = '-1' (ALL), os ports from_port e to_port devem ser 0, não 65535.
    
    Erro: updating Security Group ingress rules: from_port (0) and to_port (65535) must both be 0 to use the 'ALL' '-1' protocol!

--- infrastructure/terraform/modules/security-groups/main.tf
+++ infrastructure/terraform/modules/security-groups/main.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "eks_node" {
   ingress {
     description = "Allow nodes to communicate with each other"
     from_port   = 0
-    to_port     = 65535
+    to_port     = 0
     protocol    = "-1"
     self        = true
   }
